a1bf7cd521866a57ef175efd98eccb4cf99f2297
Adam Tindale <adam@adamtindale.com>
2017-10-20 10:35:30 -0400

    using built in repl features instead of my hack

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
+var repl = require('repl');
 var flock = require("flocking"),
         enviro = flock.init();
 var fs = require('fs');
 
-function chuck(filename){
-    fs.readFile(filename, 'utf8', function (err, data) {
-        if (err) throw err;
-        var obj = JSON.parse(data);
-        flock.synth(obj);
-    });
-}
-
-chuck('moe.json');
-chuck('curly.json');
-chuck('moe.json');
-
-/*
-setTimeout( function(){
-    chuck('moe.json');
-}, 1000);
-*/
-
 enviro.play();
+
+var replServer = repl.start({
+    prompt: "flocking |:::| ",
+});
+
+replServer.context.flock = flock;
